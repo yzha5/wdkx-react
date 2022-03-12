44c68dfe33348529fc6ce1262360bd3f0204a123
yzha5 <yzha5@163.com>
2022-03-12 11:48:13 +0800

    [document] update

--- packages/wdkx-document/layouts/document.tsx
+++ packages/wdkx-document/layouts/document.tsx
@@ -7,12 +7,44 @@ import {
     Avatar,
     Button,
     Container,
+    List,
+    ListItem,
+    ListTitle,
     Navbar,
     Toolbar,
     View,
 } from '@wdkx/component-react'
 import { Logo } from '../components/logo'
 import { Moon, Sun } from '@wdkx/icon-react'
+import styled from '@emotion/styled'
+
+const StyledLeftSide = styled.div({
+    position: 'fixed',
+    width: '16rem',
+    height: 'calc(100vh - 3.5rem)',
+    borderRight: 'solid 1px #eee',
+    padding: '1rem',
+    overflow: 'auto',
+    boxShadow: `0 .5rem 3rem rgba(0,0,0,0.1), 0 .25rem .75rem rgba(0,0,0,0.1)`,
+})
+
+const routes = [
+    { label: 'avatar', url: '/components/avatar' },
+    { label: 'backplate', url: '/components/backplate' },
+    { label: 'badge', url: '/components/badge' },
+    { label: 'button', url: '/components/button' },
+    { label: 'container', url: '/components/container' },
+    { label: 'folding', url: '/components/folding' },
+    { label: 'grid', url: '/components/grid' },
+    { label: 'input', url: '/components/input' },
+    { label: 'list', url: '/components/list' },
+    { label: 'navbar', url: '/components/navbar' },
+    { label: 'number field', url: '/components/number-field' },
+    { label: 'sidebar', url: '/components/sidebar' },
+    { label: 'text field', url: '/components/text-field' },
+    { label: 'toolbar', url: '/components/toolbar' },
+    { label: 'view', url: '/components/view' },
+]
 
 const DocumentLayout: FC = ({ children }) => {
     const [scheme, setScheme] = useState<'light' | 'dark'>('light')
@@ -23,6 +55,10 @@ const DocumentLayout: FC = ({ children }) => {
     const toggleScheme = () => {
         dispatch(setSchemeAction(scheme !== 'dark'))
         // setScheme(scheme === 'dark' ? 'light' : 'dark')
+    }
+
+    const goto = (url: string) => {
+        router.push(url)
     }
 
     useEffect(function () {
@@ -58,13 +94,13 @@ const DocumentLayout: FC = ({ children }) => {
     return (
         <ThemeProvider theme={{ palette: { scheme: scheme } }}>
             <View>
-                <Navbar color='primary' fixed frosted shadowed>
+                <Navbar color='white' light fixed frosted shadowed>
                     <Toolbar>
                         <div className='display-flex align-items-center'>
                             <Avatar size='sm'>
                                 <Logo />
                             </Avatar>
-                            <span className='margin-left-2'>维度科学</span>
+                            <span className='margin-left-2'>Documentation</span>
                         </div>
                         <div>
                             <Toolbar>
@@ -84,8 +120,25 @@ const DocumentLayout: FC = ({ children }) => {
                         </div>
                     </Toolbar>
                 </Navbar>
-                <div style={{ height: '4rem' }} />
-                <Container maxWidth='lg'>{children}</Container>
+                <div style={{ height: '3.5rem' }} />
+                <div className='display-flex'>
+                    <StyledLeftSide>
+                        <List>
+                            <ListTitle>组件</ListTitle>
+                            {routes.map((v, i) => (
+                                <ListItem
+                                    key={i}
+                                    text={v.label}
+                                    onClick={() => goto(v.url)}
+                                    selected={router.pathname === v.url}
+                                />
+                            ))}
+                        </List>
+                    </StyledLeftSide>
+                    <div style={{ marginLeft: '16rem', width: '100%' }}>
+                        <Container maxWidth='lg'>{children}</Container>
+                    </div>
+                </div>
             </View>
         </ThemeProvider>
     )
