2bfef007f4853082d9536032111647fd8b30633b
yzha5 <yzha5@163.com>
2022-03-15 14:37:54 +0800

    [normalize] update

--- packages/wdkx-normalize/src/styles/base.ts
+++ packages/wdkx-normalize/src/styles/base.ts
@@ -3,13 +3,14 @@ import { GlobalProps } from '@emotion/react'
 export const base: GlobalProps['styles'] = (theme) => ({
     '*,*::before,*::after': {
         boxSizing: 'border-box',
-        // transition: 'all .05s ease', //切换动效
+        transition: 'all .1s ease', //切换动效
     },
     html: {
         fontFamily:
             '-apple-system,BlinkMacSystemFont, Arial,Helvetica, sans-serif',
+        minHeight: '100%',
     },
-    body: { margin: 0 },
+    body: { margin: 0, minHeight: '100%' },
     a: {
         textDecoration: 'none',
     },
